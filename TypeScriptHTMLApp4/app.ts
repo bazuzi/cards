4305c661bfe9ffb3521b8b9b1e24976a87954de7
Jay Bazuzi <jaybaz@microsoft.com>
2015-12-29 23:04:50 -0500

    controls for melds / discard / draw

--- TypeScriptHTMLApp4/app.ts
+++ TypeScriptHTMLApp4/app.ts
@@ -1,12 +1,21 @@
 ﻿/// <reference path="Scripts/typings/knockout/knockout.d.ts"/>
 /// <reference path="Scripts/typings/jquery/jquery.d.ts"/>
 
-interface ICard { }
+interface ICard {
+}
 
 class RegularCard implements ICard {
     constructor(public suit: CardSuit, public rank: CardRank) { }
 
+    toString(): string {
+        return CardRank[this.rank] + ' of ' + CardSuit[this.suit];
+    }
+}
 
+class Joker implements ICard {
+    toString(): string {
+        return "Joker";
+    }
 }
 
 enum CardSuit {
@@ -31,18 +40,61 @@ enum CardRank {
     King,
 }
 
+class Meld {
+    constructor(public cards: ICard[]) { }
+}
+
+class CardInHand {
+    constructor(public card: ICard) { }
+
+    public inMeld1: boolean;
+    public inMeld2: boolean;
+    public inMeld3: boolean;
+    public inMeld4: boolean;
+    public toDiscard: boolean;
+}
 
 class MyViewModel {
-    cardsInHand = ko.observableArray<ICard>([
-        new RegularCard(CardSuit.Hearts, CardRank.Ace),
-        new RegularCard(CardSuit.Hearts, CardRank.Two),
-        new RegularCard(CardSuit.Hearts, CardRank.Three),
-        new RegularCard(CardSuit.Hearts, CardRank.Four),
-    ]);
+    cardsInHand = ko.observableArray<CardInHand>();
 
     topDiscard = ko.observable<ICard>();
+
+    meldsOnTable = ko.observableArray<Meld>();
+
+    drawFromStock = () => { };
+    drawFromDiscard = () => { };
+    discard = () => { };
 }
 
 $(() => {
-    ko.applyBindings(new MyViewModel());
+    var viewModel = new MyViewModel();
+
+    InsertTestData(viewModel);
+
+    ko.applyBindings(viewModel);
 });
+
+function InsertTestData(viewModel: MyViewModel) : void{
+    var cardInHand = new CardInHand(new RegularCard(CardSuit.Hearts, CardRank.Ace));
+    cardInHand.inMeld1 = cardInHand.inMeld2 = cardInHand.inMeld3 = cardInHand.inMeld4 = cardInHand.toDiscard = true;
+    viewModel.cardsInHand([
+        cardInHand,
+        new CardInHand(new RegularCard(CardSuit.Hearts, CardRank.Two)),
+        new CardInHand(new RegularCard(CardSuit.Hearts, CardRank.Three)),
+        new CardInHand(new RegularCard(CardSuit.Hearts, CardRank.Four)),
+    ]);
+    viewModel.topDiscard(new RegularCard(CardSuit.Clubs, CardRank.Nine));
+    viewModel.meldsOnTable([
+        new Meld([
+            new RegularCard(CardSuit.Hearts, CardRank.Two),
+            new RegularCard(CardSuit.Hearts, CardRank.Three),
+            new RegularCard(CardSuit.Hearts, CardRank.Four),
+        ]),
+        new Meld([
+            new RegularCard(CardSuit.Spades, CardRank.Two),
+            new RegularCard(CardSuit.Spades, CardRank.Three),
+            new RegularCard(CardSuit.Spades, CardRank.Four),
+        ]),
+    ]);
+
+}
